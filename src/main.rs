c2a2fc521fe777cea76f25c096bc074450fbe755
backtrac3r <medvedosandr@gmail.com>
2024-04-23 00:11:28 +0300

    .

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,6 @@ use {defmt_rtt as _, panic_probe as _};
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let p = embassy_stm32::init(Config::default());
-
     unwrap!(spawner.spawn(driver()));
 }
 
